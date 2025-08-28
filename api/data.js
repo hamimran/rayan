28073cfbf01f40e40d14f99931e577a85688af6f
hamimran <hamoozimran340@gmail.com>
2025-08-29 00:55:24 +0300

    Update data.js

--- api/data.js
+++ api/data.js
@@ -29,11 +29,12 @@ html: `
 try {
 await transporter.sendMail(mailOptions);
 // ترسل JSON للعميل ليقوم بالتحويل
-res.status(200).json({ success: true, redirect: "../reservation/index.html" });
+res.status(200).json({ success: true, redirect: "/reservation/index.html" });
 } catch (error) {
 res.status(500).json({ success: false, error: error.message });
 }
 } else {
 res.status(405).json({ error: "Method not allowed" });
 }
+
 }
